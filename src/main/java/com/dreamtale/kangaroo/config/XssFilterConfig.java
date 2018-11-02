ff67f5dec92423a574e2a4bb73759a7a82ce9951
Whaleson <1135242369@qq.com>
2018-11-02 13:43:28 +0800

    XSS attack processing

---
+++ src/main/java/com/dreamtale/kangaroo/config/XssFilterConfig.java
@@ -0,0 +1,24 @@
+package com.dreamtale.kangaroo.config;
+
+import com.dreamtale.kangaroo.core.filter.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.annotation.WebFilter;
+
+import static javax.servlet.DispatcherType.REQUEST;
+
+@Component
+public class XssFilterConfig {
+    @Bean
+    public FilterRegistrationBean registFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new XssFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("XssSqlFilter");
+        registration.setDispatcherTypes(REQUEST);
+        registration.setOrder(1);
+        return registration;
+    }
+}
---
+++ src/main/java/com/dreamtale/kangaroo/core/filter/XssFilter.java
@@ -0,0 +1,28 @@
+package com.dreamtale.kangaroo.core.filter;
+
+
+import com.dreamtale.kangaroo.core.utils.XssHttpServletRequestWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * XSS攻击过滤器
+ * Created by zchuanzhao on 2017/3/23.
+ */
+public class XssFilter implements Filter {
+    private FilterConfig filterConfig = null;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(new XssHttpServletRequestWrapper((HttpServletRequest) request), response);
+    }
+}
